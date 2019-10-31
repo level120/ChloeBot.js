1bfc145b93ed19620bd946625f3780410dee2c93
Gusam Park <tnara9573@gmail.com>
2019-10-31 22:24:41 +0900

    feat: 관리자용 명령어 추가

--- Service/command.js
+++ Service/command.js
@@ -2,15 +2,50 @@ import { RichEmbed } from 'discord.js';
 
 /**
  * bot의 명령어 집합입니다
+ * 일반 사용자용 명령어 집합은 등록하지 않습니다
  * @param { CLient } client class in discord.js
  */
 export function registrationCommand(client) {
+    const prefix = '!';
+
     client.on('message', msg => {
-        if (msg.content === 'ping') {
+        if (!msg.content.startsWith(prefix))
+            return;
+
+        if (msg.content.startsWith(prefix + 'bc') && msg.author.id === '275540899973300225') {
+            const [cmd, title, ...description] = msg.content.split(' ');
             const embed = new RichEmbed()
-                .setTitle('pong')
+                .setTitle(title)
                 .setColor(0xFF0000)
-                .setDescription('test message');
+                .setDescription(description.join(' '));
+
+            msg.channel.send(embed);
+        }
+    });
+
+    client.on('message', msg => {
+        if (!msg.content.startsWith(prefix))
+            return;
+
+        if (msg.content.startsWith(prefix + 'reg') && msg.author.id === '275540899973300225') {
+            let regList = [];
+
+            client.guilds.forEach((guild) => {
+                let defaultChannel = '';
+
+                guild.channels.forEach((channel) => {
+                    if (channel.type === 'text' && defaultChannel === '' && channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
+                        defaultChannel = channel;
+                    }
+                });
+
+                regList.push(`${guild.name} - ${defaultChannel.name}`);
+            });
+
+            const embed = new RichEmbed()
+                .setTitle("A registration guild list on the Chloe bot.")
+                .setColor(0xFF0000)
+                .setDescription(regList.join('\n'));
 
             msg.channel.send(embed);
         }
